6a250027acc88ef6dd3d822e2149da9ec7c93f24
Rudá Porto Filgueiras <rudazz@gmail.com>
2016-09-16 19:35:55 +0200

    Improve validate_id to only validate when ID is present in the request.

--- src/briefy/ws/resources/__init__.py
+++ src/briefy/ws/resources/__init__.py
@@ -56,8 +56,12 @@ class RESTService:
         :param request: pyramid request.
         :return:
         """
+        path_id = request.matchdict.get('id')
+        if path_id is None:
+            return
+
         try:
-            uuid.UUID(request.matchdict.get('id'))
+            uuid.UUID(path_id)
         except ValueError as e:
             request.errors.add('path', 'id',
                                'The id informed is not 16 byte uuid valid.')
