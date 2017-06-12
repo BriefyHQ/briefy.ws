558b587e9db87ad249a5fe77f07b2d0251d4016b
Ruda Porto Filgueiras <rudazz@gmail.com>
2017-06-12 14:53:27 +0200

    Added HistoryService and VersionService to init.

--- src/briefy/ws/resources/__init__.py
+++ src/briefy/ws/resources/__init__.py
@@ -1,4 +1,6 @@
 """Webservice resources."""
 from briefy.ws.resources.base import BaseResource  # noQA
 from briefy.ws.resources.workflow import WorkflowAwareResource  # noQA
+from briefy.ws.resources.history import HistoryService  # noQA
 from briefy.ws.resources.resource import RESTService  # noQA
+from briefy.ws.resources.versions import VersionsService  # noQA
