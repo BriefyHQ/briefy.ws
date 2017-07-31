116af5252732c75fb46daa2d4f3c6b971990d546
Érico Andrei <ericof@gmail.com>
2017-07-31 14:01:36 +0200

    Improve typing on auth package

--- src/briefy/ws/auth/__init__.py
+++ src/briefy/ws/auth/__init__.py
@@ -2,19 +2,28 @@
 from briefy.common.types import BaseUser
 from briefy.common.utils.transformers import to_serializable
 from pyramid.httpexceptions import HTTPUnauthorized as BaseHTTPUnauthorized
+from pyramid.request import Request
 from webob import Response
 
 import json
 import newrelic.agent
+import typing as t
 
 
 class AuthenticatedUser(BaseUser):
     """Class to represent current authenticated user."""
 
 
+UserOrNone = t.Union[AuthenticatedUser, None]
+
+
 @to_serializable.register(AuthenticatedUser)
-def ts_authenticated_user(val):
-    """Serializer for AuthenticatedUser."""
+def ts_authenticated_user(val: AuthenticatedUser) -> dict:
+    """Serializer for AuthenticatedUser.
+
+    :param val: AuthenticatedUser instance.
+    :return: Dictionary with AuthenticatedUser serialization.
+    """
     return val.to_dict()
 
 
@@ -29,37 +38,41 @@ class HTTPUnauthorized(BaseHTTPUnauthorized):
         self.content_type = 'application/json'
 
 
-def user_factory(request) -> AuthenticatedUser:
+def user_factory(request: Request) -> UserOrNone:
     """Create a user map from jwt token.
 
     :param request: pyramid request object.
-    :return: Authenticated user instance or None
+    :return: The AuthenticatedUser, if exists, or None (representing an AnonymousUser).
     """
     user_id = request.authenticated_userid
+    authenticated_user = None
     if user_id:
         data = request.jwt_claims
         # add all user data to the newrelic custom attributes
         newrelic.agent.add_custom_parameter('user_id', user_id)
         for key, value in data.items():
             newrelic.agent.add_custom_parameter(key, str(value))
-        return AuthenticatedUser(user_id, data)
-    else:
-        return None
+            authenticated_user = AuthenticatedUser(user_id, data)
+    return authenticated_user
 
 
-def groupfinder(userid, request):
+def groupfinder(userid: str, request: Request) -> t.Sequence[str]:
     """Callback to the authentication policy to return the list of users.
 
     :param userid: authenticatded userid.
     :param request: pyramid request object.
     :return: list of user groups
-    :rtype: list
     """
     return request.jwt_claims.get('groups', [])
 
 
-def validate_jwt_token(request, **kwargs):
-    """Use pyramid JWT to validate if the user is authenticated."""
+def validate_jwt_token(request: Request, **kwargs) -> UserOrNone:
+    """Use pyramid JWT to validate if the user is authenticated.
+
+    :param request: Pyramid request object.
+    :param kwargs: Additional keyword arguments.
+    :return: The AuthenticatedUser, if exists, or None (representing an AnonymousUser).
+    """
     user_id = request.authenticated_userid
     if user_id is None:
         raise HTTPUnauthorized
