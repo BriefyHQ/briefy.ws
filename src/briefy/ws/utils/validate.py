e71c00878df6fac9daface4ea9be344d0ecf0f70
Érico Andrei <ericof@gmail.com>
2017-08-09 11:47:24 +0200

    Fix validate_uuid checking. There are, right now, 455 entries on the database with valid UUIDs that are not uuid4

--- src/briefy/ws/utils/validate.py
+++ src/briefy/ws/utils/validate.py
@@ -1,15 +1,21 @@
 """Common validators."""
+import typing as t
 import uuid
 
 
-def validate_uuid(value: str, version: int=4) -> bool:
+def validate_uuid(value: str, version: t.Optional[int]=None) -> bool:
     """Check if id parameter is UUID valid.
 
-    :param request: pyramid request.
-    :return:
+    :param value: A string that should represent an uuid.
+    :param version: Version of the uuid to check against.
+    :return: Check if value is a valid uuid or not.
     """
     try:
-        valid = uuid.UUID(value).version == version
+        value = uuid.UUID(value)
     except (TypeError, ValueError) as e:
         valid = False
+    else:
+        valid = True
+        if version:
+            valid = value.version == version
     return valid
--- tests/utils/test_validate.py
+++ tests/utils/test_validate.py
@@ -34,8 +34,8 @@ test_data = [
     '',
     None,
     'foo',
-    '339e1685-59ef-5050-b0b3-5a8060e1f4cc',
-    '11111111-1111-1111-1111-111111111111'
+    'x39e1685-59ef-5050-b0b3-5a8060e1f4cc',
+    '11111111-s111-1111-1111-111111111111'
 ]
 
 
@@ -45,3 +45,26 @@ def test_uuid_invalid(value):
     func = validate.validate_uuid
 
     assert func(value) is False
+
+
+test_data = [
+    '197587e1-eb7d-4aa0-5ea1-aef575a28ade',
+    '74fdc63e-3b82-4e52-fd44-30fdf5b5c62e',
+    '800b12b0-6cda-4b22-c666-f9ce0a282b6b',
+    '9b13ea27-e517-4687-7aea-f17e7a10c308',
+    'cc9964da-9d11-45d0-670c-4efa8e2eaaf9',
+    'df454734-d6af-4b77-785f-5585900eb57d',
+    '10ca7fe4-da55-40ef-1b24-679524b837ac',
+    'fe055379-23f8-40bd-1a19-aca784a353f4',
+    'fe5403dd-32ab-4c69-59ae-5d1db45e34ee',
+    'ff086cf3-bcbb-49a2-6f46-76782330d6fb',
+    'ffcff482-47c3-4224-1f7e-0e5cbfc269e7'
+]
+
+
+@pytest.mark.parametrize('value', test_data)
+def test_uuid_valid_not_uuid4(value):
+    """Test validate_uuid function without specifying a version."""
+    func = validate.validate_uuid
+
+    assert func(value) is True
